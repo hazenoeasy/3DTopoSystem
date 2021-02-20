add96f532ebdda534378f28d9270232d7fa80d65
hazenoeasy <893940361@qq.com>
2021-02-20 15:52:17 +0800

    set auto formate

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App)
+  .use(store)
+  .use(router)
+  .mount('#app'); // mount 不返回app 返回跟组件实例
